6e670f4c47959382768828f3f990b42ae439e23a
fhw12 <nikita.pymitron@gmail.com>
2024-09-01 09:19:26 +0700

    get_status method

--- src/not_pixel.py
+++ src/not_pixel.py
@@ -1,9 +1,12 @@
+from models.user_status import UserStatus
+
 import aiohttp
 
 class NotPixel():
-    def __init__(self, telegramMiniAppInitData):
+    def __init__(self, telegram_mini_app_init_data, user_agent):
         self.headers = {
-            "Authorization": telegramMiniAppInitData
+            "Authorization": telegram_mini_app_init_data,
+            "User-Agent": user_agent,
         }
 
         self.session = aiohttp.ClientSession(
@@ -11,14 +14,33 @@ class NotPixel():
             headers=self.headers,
         )
 
-    async def get_status():
+    async def get_status(self) -> UserStatus | None:
+        user_status_raw = await self.session.request(
+            url="/api/v1/mining/status",
+            method="GET",
+        )
+
+        if user_status_raw.status != 200:
+            return None
+
+        user_status = await user_status_raw.json()
+
+        status_model = UserStatus(
+            coins = user_status['coins'],
+            claimed = user_status['claimed'],
+            total_user_pixels = user_status['totalUserPixels'],
+            user_balance = user_status['userBalance'],
+            charges = user_status['charges'],
+            max_charges = user_status['maxCharges'],
+        )
+
+        return status_model
+
+    async def get_pixel(self):
         return NotImplemented
 
-    async def get_pixel():
+    async def set_pixel(self, pixel_id, color):
         return NotImplemented
 
-    async def set_pixel():
-        return NotImplemented
-
-    async def claim():
+    async def claim(self):
         return NotImplemented
